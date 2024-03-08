62dbbccb0f743b96999fdc4c0c32cc71617be444
Nadiia <shulltea113@gmail.com>
2024-03-08 22:11:00 +0200

    generate vite project

---
+++ homeworks_128/lesson_11/src/components/common/SiteHeader.jsx
@@ -0,0 +1,15 @@
+import './../../assets/scss/common/header.scss'
+export default function SiteHeader(){
+    <header>
+        <div className="container">
+            <div className="logo">
+                <a href="/">Logo</a>
+            </div>
+            <nav className="main-menu">
+                <ul>
+                    <li><a href="#">home</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+}
